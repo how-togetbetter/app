f8bfe0a7091cf3f97fffc0077f0bcaedc642a953
root <root@DESKTOP-IINT55K.localdomain>
2020-07-21 14:28:35 -0700

    .env and route update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,8 +11,19 @@ app.use(bodyParser.json());
 const PORT = 3000 || process.env.PORT;
 
 // Routes
-// app.get("/", (req, res) => {
-// });
+app.get("/", (req, res) => {
+  axios
+    .get(
+      `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&order=date&maxResults=${maxResults}`
+    )
+    .then(function (response) {
+      //console.log(response.data.items);
+      res.send(response.data.items);
+    })
+    .catch(function (error) {
+      res.status(500).send(error);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
